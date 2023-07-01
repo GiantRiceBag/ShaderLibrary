02be9d3193cb3309ada5e9b654ae80a79b1d0c66
GiantRiceBag <787121798@qq.com>
2023-07-01 20:20:39 +0800

    feat:Quad网格的湖泊模拟

---
+++ Assets/Demo/6-QuadLake/Lake.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Lake : MonoBehaviour
+{
+    Camera cam => Camera.main;
+    public MeshRenderer meshRenderer;
+
+    private void OnRectTransformDimensionsChange()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnGUI()
+    {
+        
+    }
+}
